28d54e0f4946d467df7ebebedbd8e63ec0a818da
cauwt <cauwt@hotmail.com>
2017-11-14 13:45:23 +0800

    refactor aggregateBySession

--- src/main/java/com/ibeifeng/sparkproject/spark/UserVisitSessionAnalysisSpark.java
+++ src/main/java/com/ibeifeng/sparkproject/spark/UserVisitSessionAnalysisSpark.java
@@ -6,15 +6,13 @@ import com.ibeifeng.sparkproject.constant.Constants;
 import com.ibeifeng.sparkproject.dao.ITaskDAO;
 import com.ibeifeng.sparkproject.dao.impl.DAOFactory;
 import com.ibeifeng.sparkproject.domain.Task;
-import com.ibeifeng.sparkproject.jdbc.JDBCHelper;
+import com.ibeifeng.sparkproject.util.DateUtils;
 import com.ibeifeng.sparkproject.util.ParamUtils;
 import com.ibeifeng.sparkproject.util.StringUtils;
 import com.ibeifeng.sparkproject.util.ValidUtils;
-import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.Dataset;
@@ -23,12 +21,9 @@ import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.hive.HiveContext;
 import scala.Tuple2;
-import scala.xml.PrettyPrinter;
 
-import java.util.HashSet;
+import java.util.Date;
 import java.util.Iterator;
-import java.util.Set;
-import java.util.function.Consumer;
 
 /**
  * Created by zkpk on 11/5/17.
@@ -42,6 +37,7 @@ public class UserVisitSessionAnalysisSpark {
                 .getOrCreate();
 
         mockData(spark);
+
         ITaskDAO taskDAO = DAOFactory.getTaskDAO();
         long taskid = ParamUtils.getTaskIdFromArgs(args);
 
@@ -49,17 +45,16 @@ public class UserVisitSessionAnalysisSpark {
         Task task = taskDAO.findById(taskid);
         JSONObject taskParam = JSONObject.parseObject(task.getTaskParam());
         JavaRDD<Row> actionRDD = getActionRDD(spark,taskParam);
-        System.out.println("#actionRDD: " + actionRDD.count());
         //<sessionid,<sessionid,searchKeywords,clickCategoryIds,age,professional,city,sex>>
         JavaPairRDD<String,String> sessionid2AggrInfoRDD =
                 aggregateBySession(spark,actionRDD);
-        System.out.println(sessionid2AggrInfoRDD.count());
         sessionid2AggrInfoRDD.take(10).forEach(tuple -> System.out.println(tuple._2));
 
         JavaPairRDD<String,String> filteredSessionid2AggrInfoRDD =
                 filterSession(sessionid2AggrInfoRDD,taskParam);
-        System.out.println(filteredSessionid2AggrInfoRDD.count());
         filteredSessionid2AggrInfoRDD.takeSample(false,10).forEach(tuple -> System.out.println(tuple._2));
+
+        // ratio for each kind of sessions
 
 
         spark.stop();
@@ -94,20 +89,20 @@ public class UserVisitSessionAnalysisSpark {
         //map to <sessionID, Row>
         JavaPairRDD<String, Row> sessionid2ActionRDD = actionRDD.mapToPair(
                 (PairFunction<Row, String, Row>) row -> new Tuple2<>(row.getString(2),row));
-        //debug
-        //System.out.println("#sessionid2ActionRDD:"+ sessionid2ActionRDD.count());
         JavaPairRDD<String, Iterable<Row>> sessionid2ActionsRDD =  sessionid2ActionRDD.groupByKey();
-        //debug
-        //System.out.println("#sessionid2ActionsRDD:"+ sessionid2ActionRDD.count());
 
         //<userid, partAggrInfo(sessionid, searchKeywords, clickCategoryIds)>
         JavaPairRDD<Long, String> userid2PartAggrInfoRDD =  sessionid2ActionsRDD.mapToPair(
                 (PairFunction<Tuple2<String,Iterable<Row>>, Long, String> ) tuple -> {
                     String sessionid = tuple._1;
-                    Long userid = null;
                     Iterator<Row> iterator = tuple._2.iterator();
                     StringBuffer searchKeywordsBuffer = new StringBuffer("");
                     StringBuffer clickCategoryIdsBuffer = new StringBuffer("");
+
+                    Long userid = null;
+                    Date startTime = null;
+                    Date endTime = null;
+                    int stepLength = 0;
 
                     while(iterator.hasNext()){
                         Row row = iterator.next();
@@ -137,40 +132,51 @@ public class UserVisitSessionAnalysisSpark {
                             }
                         }
 
+                        // get the startTime and endTime of a session
+                        Date actionTime = DateUtils.parseTime(row.getString(4));
+                        if(startTime == null){
+                            startTime = actionTime;
+                        }
+                        if(endTime == null){
+                            endTime = actionTime;
+                        }
+                        if(actionTime.before(startTime)){
+                            startTime = actionTime;
+                        }
+                        if(actionTime.after(endTime)){
+                            endTime = actionTime;
+                        }
+
+                        // get step length
+                        stepLength ++;
+
+
                     }
+                    // calculate visit_length (in second)
+                    long visitLength = (endTime.getTime() - startTime.getTime())/1000;
+
                     String searchKeywords = StringUtils.trimComma(searchKeywordsBuffer.toString());
                     String clickCategoryIds = StringUtils.trimComma(clickCategoryIdsBuffer.toString());
 
                     // return <userid, partAggrInfo>
 
                     String partAggrInfo = Constants.FIELD_SESSION_ID+"="+sessionid
-                            + (StringUtils.isEmpty(searchKeywords)? "" : "|"+Constants.FIELD_SEARCH_KEYWORDS+"="+searchKeywords)
-                            + (StringUtils.isEmpty(clickCategoryIds)? "" : "|"+Constants.FIELD_CLICK_CATEGORY_IDS+"="+clickCategoryIds);
+                            + "|"+Constants.FIELD_SEARCH_KEYWORDS+"="+searchKeywords
+                            + "|"+Constants.FIELD_CLICK_CATEGORY_IDS+"="+clickCategoryIds
+                            + "|"+Constants.FIELD_VISIT_LENGTH +"="+visitLength
+                            + "|"+Constants.FIELD_STEP_LENGTH +"="+stepLength;
                     return new Tuple2<>(userid,partAggrInfo);
                 });
-        //debug
-        //System.out.println(userid2PartAggrInfoRDD.count());
 
 
         String sql = "select user_id,username,name,age,professional,city,sex from user_info";
         JavaRDD<Row> userInfoRDD = spark.sql(sql).toJavaRDD();
         JavaPairRDD<Long,Row> userid2InfoRDD = userInfoRDD.mapToPair((PairFunction<Row,Long,Row>) (Row row) ->
                 new Tuple2<>(row.getLong(0),row)) ;
-        //debug
-//        userid2InfoRDD.takeSample(false,10).forEach(action -> System.out.println(action._1
-//                + ", info: "+action._2.toString()
-//        ));
 
         //join sessionAggr and user
         // return <userid, <partAggrInfo, userFullInfo>>
         JavaPairRDD<Long,Tuple2<String,Row>> userid2FullInfoRDD = userid2PartAggrInfoRDD.join(userid2InfoRDD);
-        //debug
-        //System.out.println(userid2FullInfoRDD.count());
-//        userid2FullInfoRDD.takeSample(false,10).forEach(action -> System.out.println(
-//                "userid: "+ action._1
-//                + ", aggr: "+action._2._1
-//                + ", info: "+action._2._2.toString()
-//        ));
 
         //return <sessionid,fullAggrInfo(age,professional,city,sex)>
         JavaPairRDD<String, String> sessionid2FullAggrInfoRDD = userid2FullInfoRDD.mapToPair(
